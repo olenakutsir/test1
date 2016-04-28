62c95160558756a1fc0a4d0b6d57a9784476846a
olenakutsir <olena.kutsir@gmail.com>
2016-04-28 16:46:08 +0000

    j

---
+++ js/skript.js
@@ -0,0 +1,14 @@
+$('#btn-send').click(function() {
+  // Please set your email in url field
+  $.ajax({
+      url: "https://formspree.io/YOUR_EMAIL@HERE",  
+      method: "POST",
+      data: {
+             email: email.value,
+             message: msg.value
+             },
+      dataType: "json"
+  }).done(function() {
+     $('#container').html('<h1>Thank you!</h1>')
+  } );
+});
